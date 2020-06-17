096dff5dcb789ec521355c2ad940683e043193a7
Eduardo <eddeav98@gmail.com>
2020-06-17 01:30:22 -0500

    Desplegado de Banner en LCD utilizando Driver

---
+++ MKL25Z128xxx4_Banner_Banner/source/MKL25Z128xxx4_Banner_Banner.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MKL25Z4.h"
+#include "fsl_debug_console.h"
+#include "LCD.h"
+
+
+int main(void) {
+
+
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+    BOARD_InitDebugConsole();
+
+    LCD_Activate(1u,1u,0u);
+    LCD_Set(1u,0u,0u);
+    LCD_Clear();
+
+
+
+
+
+
+    while(1) {
+    	Line(1);
+        LCD_Mode(1, 1);
+    	        LCD_Write('M');
+    	        LCD_Write('I');
+    	        LCD_Write('C');
+    	        LCD_Write('R');
+    	        LCD_Write('O');
+    	        LCD_Write('S');
+    	        LCD_Write(' ');
+    	        LCD_Write(' ');
+    	        LCD_Write('I');
+    	        LCD_Write('E');
+    	        LCD_Write('I');
+    	        LCD_Write(' ');
+    	        LCD_Write(' ');
+    	        LCD_Write('U');
+    	        LCD_Write('A');
+    	        LCD_Write('Z');
+    }
+    return 0 ;
+}
